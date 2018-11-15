191bd70333542820063f8147579c42e9b43de021
Azza276 <Azza276@users.noreply.github.com>
2018-11-15 21:30:44 +1000

    Test 3

---
+++ OGN_DCS_Mod_Sync/OGN_DCS_Mod_Sync/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OGN_DCS_Mod_Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
