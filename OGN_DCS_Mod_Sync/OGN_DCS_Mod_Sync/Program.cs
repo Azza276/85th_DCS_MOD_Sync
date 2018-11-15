04714ca2b7d64ab2dd7e6421bf0542e4ef6f93c0
Azza276 <Azza276@users.noreply.github.com>
2018-11-15 22:44:46 +1000

    Playing

--- OGN_DCS_Mod_Sync/OGN_DCS_Mod_Sync/Program.cs
+++ OGN_DCS_Mod_Sync/OGN_DCS_Mod_Sync/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace OGN_DCS_Mod_Sync
 {
@@ -6,7 +7,23 @@ namespace OGN_DCS_Mod_Sync
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Determine DCS Users Directory//
+            // Find the directory path from C:\Users\[Username]\Saved Games\DCS or DCS Openbeta or DCS World or DCS Openalpha//
+            // Copy that directory path into the "userpath" variable
+
+            string userName = (Environment.UserName);
+            string partialName = "Liveries";
+            DirectoryInfo driveToSearch = new DirectoryInfo(@"c:\\Users\\" + userName + "\\");
+            FileInfo[] filesInDir = driveToSearch.GetFiles("*" + partialName);
+
+            foreach (FileInfo foundFile in filesInDir)
+            {
+                string userPath = foundFile.FullName;
+                Console.WriteLine(userPath);
+            }
+
+
+            // Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
     }
